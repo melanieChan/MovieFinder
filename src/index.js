2f8dfc52e4521259346006f851eaa5f0a6c67fe2
Melanie Chan <melanie.chan@sjsu.edu>
2021-06-03 14:36:42 -0700

    added search bar, list, and app components

--- src/index.js
+++ src/index.js
@@ -2,16 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+
+function App() {
+  return (
+    <div className="app">
+      <SearchBar />
+      <MovieList />
+    </div>
+  );
+}
+
 function SearchBar() {
   return (
     <div id="search">
       <h1>Movies</h1>
-      <input id="searchBar" type="text" placeholder="Search Movies"/>
+      <input
+        id="searchBar"
+        type="text"
+        placeholder="Search Movies"
+      />
+      <button className="searchBtn">Find Movies</button>
     </div>
     );
 }
 
+function MovieList() {
+  return (
+    <div className="movieList">
+    </div>
+  );
+}
+
 ReactDOM.render(
-  <SearchBar />,
+  <App />,
   document.getElementById('root')
 );
