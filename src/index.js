e714c13aa5cb28c25223cbbc7b573cd5c55231ac
Melanie Chan <melanie.chan@sjsu.edu>
2021-06-03 16:09:54 -0700

    installed mui to use card and grid components

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+import Grid from "@material-ui/core/Grid"
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
 
 function App() {
   return (
@@ -29,6 +32,39 @@ function SearchBar() {
 function MovieList() {
   return (
     <div className="movieList">
+      <MovieCard/>
+      <MovieCard/>
+
+    </div>
+  );
+}
+
+function MovieCard() {
+  return (
+    <div >
+      <Card  style={{backgroundColor: 'transparent', margin: "25px", borderRadius: "20px"}}>
+        <CardActionArea >
+          <Grid container
+            className="cardGridContainer"
+            direction = "column"
+            justify = "center"
+            spacing = {2} >
+
+            <Grid item className="movieTitle">Movie Name</Grid>
+
+            <Grid container
+              direction='row'
+              justify = "center"
+              spacing = {10}>
+              <Grid item>release data</Grid>
+              <Grid item>runtime</Grid>
+              <Grid item>genre</Grid>
+              <Grid item>director</Grid>
+            </Grid>
+
+          </Grid>
+        </CardActionArea>
+      </Card>
     </div>
   );
 }
