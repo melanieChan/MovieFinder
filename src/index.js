f3bba492c42355fb4841dc9978e83e02586b2cb0
Melanie Chan <melanie.chan@sjsu.edu>
2021-06-04 14:47:46 -0700

    fetched and displayed movie data
    for hard coded search query

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {useState} from "react";
+import {useEffect} from "react";
 
 import Grid from "@material-ui/core/Grid"
 import Card from "@material-ui/core/Card";
@@ -24,20 +26,46 @@ function SearchBar() {
         type="text"
         placeholder="Search Movies"
       />
-      <button className="searchBtn">Find Movies</button>
+      <button className="searchBtn">Find Movie</button>
     </div>
     );
 }
 
+// list of movies from search results
 function MovieList() {
+  const [movies, setMovies] = useState(null);
+
+    const movieTitle = "moana";
+
+    // search movies
+    useEffect(()=> {
+        fetch(`http://www.omdbapi.com/?s=${movieTitle}&apikey=52514a3a`)
+          .then(res => res.json())
+          .then(setMovies)
+          .catch(console.error);
+    }, []);
+
+    // display search results
+    if (movies) {
+      // display a card for every movie in search results list
+      return (
+           <div className="movieList">
+             {movies.Search.map(movie => {
+                return <MovieCard name={ movie.Title}
+                              releaseDate={movie.Year} runtime="1 hr" genre="family" director="director"/>
+              })}
+           </div>
+         );
+    }
+
   return (
     <div className="movieList">
-      <MovieCard name="Movie"releaseDate="6/2/21" runtime="1 hr" genre="family" director="director"/>
-      <MovieCard name="Movie" releaseDate="6/2/21" runtime="1 hr" genre="family" director="director"/>
+      <h2>No results</h2>
     </div>
   );
 }
 
+// card showing details of a specific movie
 function MovieCard({name, releaseDate, runtime, genre, director}) {
   return (
     <div >
